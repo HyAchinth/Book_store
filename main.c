d552e9336f62d6c9d33d5065f92c67bad2b41950
PapaSmurf2k18 <achinth.s@gmail.com>
2018-09-28 13:36:49 +0000

    update 2

--- main.c
+++ main.c
@@ -1,19 +1,29 @@
-#include <stdio.h>
-/*typedef struct{
-	int isbnstatus;
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct{
+	int isbn,status;
 	char bname[30],aname[30];
-}book;*/
+}book;
 
 int main()
 {
  	int choice;
+start:	printf("\n1.Add\n2.Exit\n3.view\n");
 	printf("Enter your choice:");
 	scanf("%d",&choice);
 	switch(choice)
 	{
-		case 1:printf("\ngood");
+		case 1:printf("\nEnter the Book name:");
+			book name;
+			scanf("%s",name.bname);
+			printf("\nEnter isbn:");
+			scanf("%d",&(name.isbn));			
 			break;
-		default: printf("\nbad");
-	}		
+		case 2:exit(0);
+		case 3:printf("\nBook name:%s\nisbn:%d",name.bname,name.isbn);
+			break;
+		default: printf("\nwrong input");
+	}
+	goto start;		
 	return 0;
 }
