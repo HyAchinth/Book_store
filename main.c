771404e9538e2db5220bede5dd5c03f1ce1d9362
PapaSmurf2k18 <achinthyas.cs18@rvce.edu.in>
2018-09-28 19:13:26 +0530

    add head

--- main.c
+++ main.c
@@ -1,23 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
-typedef struct{
-	int isbn,status;
-	char bname[30],aname[30];
-}book;
+#include "main.h"
 
 int main()
 {
  	int choice;
-start:	printf("\n1.Add\n2.Exit\n3.view\n");
+	book name[50];
+	start:	
+	printf("\n1.Add\n2.Exit\n3.view\n");
 	printf("Enter your choice:");
 	scanf("%d",&choice);
 	switch(choice)
 	{
-		case 1:printf("\nEnter the Book name:");
-			book name;
-			scanf("%s",name.bname);
-			printf("\nEnter isbn:");
-			scanf("%d",&(name.isbn));			
+		case 1:
+			for(int i=0;i<5;i++){
+				printf("\n%d:\nEnter the Book name:",(i+1));
+				
+				scanf("%s",name[i].bname);
+				printf("\nEnter isbn:");
+				scanf("%d",&(name[i].isbn));
+			}
 			break;
 		case 2:exit(0);
 		case 3:printf("\nBook name:%s\nisbn:%d",name.bname,name.isbn);
---
+++ main.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct{
+	int isbn,status;
+	char bname[30],aname[30];
+}book;
+
