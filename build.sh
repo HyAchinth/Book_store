1dcdf5f5d8956a9f8e080d1c4348e75d4527f98f
PapaSmurf2k18 <achinth.s@gmail.com>
2018-09-28 12:37:36 +0000

    update

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cc main.c -o build.out
--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+/*typedef struct{
+	int isbnstatus;
+	char bname[30],aname[30];
+}book;*/
+
 int main()
 {
  	int choice;
-	typedef struct{
-			int isbnstatus;
-			char bname[30],aname[30];
-	}book;
-	
 	printf("Enter your choice:");
-	scanf("%d",choice);
-	switch choice
+	scanf("%d",&choice);
+	switch(choice)
 	{
 		case 1:printf("\ngood");
+			break;
+		default: printf("\nbad");
 	}		
 	return 0;
 }
