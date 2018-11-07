201656097bab50bb7347db2f3a651b6493774cc5
Christopher Rose <christophervrose@gmail.com>
2018-11-07 12:54:27 +0530

    Add duct tape

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cc main.c -o build.out
+cc new.c -o build.out
--- new.c
+++ new.c
@@ -6,7 +6,7 @@
 library_s library;
 receipt_list_s receipt_list;
 receipt_s receipt_temp;
-
+int library_slice[BOOK_MAX];
 
 int if_exists(const char *fname){
 	FILE *file;
@@ -19,6 +19,25 @@ int if_exists(const char *fname){
 }
 
 
+int make_library(){
+	int n;
+	int i=1;
+	while(true){
+		printf("Enter book name\n:");
+		scanf("%[^\n]",library.books[i-1].bname);
+		printf("Enter book price\n:");
+		scanf("%f",&(library.books[i-1].cost) ); 
+		
+		
+		printf("Continue? (Y/N)");
+		if(getchar()=='N'){
+			library.number_of_books = i;
+			break;
+		}
+		i++;
+	}
+	return 0;
+}
 
 
 int load_library(){
@@ -26,6 +45,11 @@ int load_library(){
 		FILE *fp = fopen("library.dat","rb");
 		fread(&library,sizeof(library_s),1,fp);
 		fclose(fp);
+		
+		int i;
+		for(i=0;i<number_of_books;i++)
+			library_slice[i] = i;
+		library_slice[i] = -1;
 		return 1;
 	}
 	else return 0;
@@ -33,12 +57,18 @@ int load_library(){
 
 
 int save_library(){
-	FILE *fp = fopen("company.details","wb+");
+	FILE *fp = fopen("library.dat","wb+");
 	fwrite(&library,sizeof(library_s),1,fp);
 	fclose(fp);
 	return 1;
 }
 
+
+void print_slice(){
+	for(int i=0;i<slices_len();i++){
+		printf("%d %s %f \n",i+1,library.books[ library_slice[i] ].bname, library.books[ library_slice[i] ].cost );
+	}
+}
 
 int load_receipt_list(){
 	if(exists("receipt.list.dat")){
@@ -62,32 +92,96 @@ int save_receipt(char *name){
 
 
 
+int slices_len(){
+	int i;
+	for(i=0;i[library_slice]!=-1;i++);
+	return i;
+}
 
-int main()
+int slices_insert(int *slice_array, int element){
+	if(slices_len(slice_array)==BOOK_MAX-1)
+		return 1;
+	else{
+		slice_array[slices_len(slice_array)+1] = -1;
+		slice_array[slices_len(slice_array)] = element;
+		
+	}
+		
+}
+
+int slices_remove(int *slice_array){
+	if(slices_len(slice_array)>0){
+	}
+}
+
+
+/*int main()
 {
-int choice,input;
-char name[50];
-load_receipt_list();
-printf("Welcome to XYZ bookstore!\n");
-printf("Enter your choice:\n1.Buy books /. /\n2.Exit\n");
-scanf("%d",&choice};
-switch(choice){
-	case 1: printf("Enter mode of search:\n1.Book Title\n2.Author\n3.Publisher\n4.Category\n");
+	int choice,input;
+	char name[STR_LENGTH];
+	load_receipt_list();
+	int library_slice[BOOK_MAX];
+	
+	
+	printf("Welcome to XYZ bookstore!\n");
+	printf("Enter your choice:\n1.Buy books /. /\n2.Exit\n");
+	scanf("%d",&choice};
+	
+	
+	switch(choice){
+		case 1: 
+			printf("Enter mode of search:\n1.Book Title\n2.Author\n3.Publisher\n4.Category\n");
 			scanf("%d",&input);
+			
 			switch(input){
 				case 1: printf("Enter name of the Book:");
-						scanf("%s",name);
-						break;
+					scanf("%s",name);
+					break;
 				case 2: printf("Enter name of the Author:");
-						scanf("%s",name);
-						break;
+					scanf("%s",name);
+					break;
 				case 3: printf("Enter name of the Publisher\n4:");
-						scanf("%s",name);
-						break;
+					scanf("%s",name);
+					break;
 				case 4: printf("Enter Category of the book:\n1)Chemistry\n2)Math\n3)Physics");
-						scanf("%s",name);
-						break;
+					scanf("%s",name);
+					break;
 				default : printf("Wrong input!");
 			}
 			search(name);
 			
+
+			*/
+			
+int main(){
+	load_receipt_list();int choice;
+	
+	
+	
+	
+	printf("1. Save 2. Load 3. Make 4. Print");
+	scanf("%d",&choice);
+	switch(choice){
+		case 1:
+			save_library();
+			break;
+		case 2:
+			load_library();
+			break;
+		case 3:
+			make_library();
+			break;
+		case 4:
+			print_slice();
+			break;
+		default:
+	}
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
